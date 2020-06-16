f27130791f50ec0ab45cfb2629239853f18862ef
Yulian Ashikov <u.ashikov@abv.bg>
2020-06-16 23:54:34 +0300

    Set up db integration, set up user password options. Added connection string to the app settings.

--- Server/Exchangerat/Startup.cs
+++ Server/Exchangerat/Startup.cs
@@ -1,7 +1,11 @@
 namespace Exchangerat
 {
+    using Data;
+    using Data.Models;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
@@ -10,18 +14,30 @@ namespace Exchangerat
     {
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            this.Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ExchangeratDbContext>(options =>
+                options.UseSqlServer(this.Configuration.GetConnectionString("ExchangeratDbConnection")));
+
+            services
+                .AddIdentity<User, IdentityRole>(options =>
+                {
+                    options.Password.RequireDigit = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequiredLength = 6;
+                })
+                .AddEntityFrameworkStores<ExchangeratDbContext>();
+
             services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -31,6 +47,7 @@ namespace Exchangerat
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
