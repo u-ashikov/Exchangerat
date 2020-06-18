2057567db7a7f1c3afe157bb1c9a9fd47dcce39f
Yulian Ashikov <u.ashikov@abv.bg>
2020-06-19 00:00:17 +0300

    Created services project. Added user service with methods for authenticating an user and for getting all users. Added service model for the user after authentication.

---
+++ Server/Exchangerat.Services/Contracts/IUserService.cs
@@ -0,0 +1,14 @@
+﻿namespace Exchangerat.Services.Contracts
+{
+    using Data.Models;
+    using Models;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public interface IUserService
+    {
+        IEnumerable<User> GetAll();
+
+        Task<UserModel> Authenticate(string username, string password);
+    }
+}
---
+++ Server/Exchangerat.Services/Models/UserModel.cs
@@ -0,0 +1,22 @@
+﻿namespace Exchangerat.Services.Models
+{
+    using Exchangerat.Data.Models;
+
+    public class UserModel
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Token { get; set; }
+
+        public UserModel(User user, string token)
+        {
+            Id = user.Id;
+            FirstName = user.FirstName;
+            LastName = user.LastName;
+            Username = user.UserName;
+            Token = token;
+        }
+    }
+}
