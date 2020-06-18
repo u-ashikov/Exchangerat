40a47c5cafbf593ac2d76e714941129f27976966
Yulian Ashikov <u.ashikov@abv.bg>
2020-06-19 00:01:33 +0300

    Set up JWT authentication. Registered user service.

--- Server/Exchangerat/Startup.cs
+++ Server/Exchangerat/Startup.cs
@@ -1,8 +1,10 @@
 namespace Exchangerat
 {
+    using Common.Helpers;
     using Data;
     using Data.Models;
     using Infrastructure.Extensions;
+    using Microsoft.AspNetCore.Authentication.JwtBearer;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.AspNetCore.Identity;
@@ -10,6 +12,10 @@ namespace Exchangerat
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
+    using Microsoft.IdentityModel.Tokens;
+    using Services.Contracts;
+    using Services.Implementations;
+    using System.Text;
 
     public class Startup
     {
@@ -36,6 +42,32 @@ namespace Exchangerat
                 })
                 .AddEntityFrameworkStores<ExchangeratDbContext>();
 
+            var appSettingsSection = this.Configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
+
+            var appSettings = appSettingsSection.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
+            services.AddTransient<IUserService, UserService>();
+
             services.AddControllers();
         }
 
@@ -50,6 +82,13 @@ namespace Exchangerat
             }
 
             app.UseRouting();
+
+            app.UseCors(policy =>
+            {
+                policy.AllowAnyHeader();
+                policy.AllowAnyMethod();
+                policy.AllowAnyOrigin();
+            });
 
             app.UseAuthentication();
             app.UseAuthorization();
