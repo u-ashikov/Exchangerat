07bb1b178314088b94a3b28405a65456e0a7f904
Yulian Ashikov <u.ashikov@abv.bg>
2020-06-19 00:02:24 +0300

    Added users controller with 2 endpoints for testing if the JWT authentication works. Added request model for accepting username and password from the client.

---
+++ Server/Exchangerat/Controllers/UsersController.cs
@@ -0,0 +1,41 @@
+﻿namespace Exchangerat.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using Models;
+    using Services.Contracts;
+    using System.Threading.Tasks;
+
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService userService;
+
+        public UsersController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        [HttpPost("Authenticate")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Authenticate([FromBody] RequestModel model)
+        {
+            var result = await this.userService.Authenticate(model.Username, model.Password);
+
+            if (result == null)
+            {
+                return this.BadRequest(new {message = "Bad Request"});
+            }
+
+            return this.Ok(result);
+        }
+
+        [HttpGet("GetAll")]
+        public IActionResult GetAll()
+        {
+            return this.Ok(this.userService.GetAll());
+        }
+    }
+}
---
+++ Server/Exchangerat/Models/RequestModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Exchangerat.Models
+{
+    public class RequestModel
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
