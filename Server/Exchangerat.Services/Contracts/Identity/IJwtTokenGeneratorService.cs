8989040ea4e7297262306003b0999054e993db4f
Yulian Ashikov <u.ashikov@abv.bg>
2020-06-19 22:38:50 +0300

    Extracted the Jwt token generation in separate service.

---
+++ Server/Exchangerat.Services/Contracts/Identity/IJwtTokenGeneratorService.cs
@@ -0,0 +1,9 @@
+﻿namespace Exchangerat.Services.Contracts.Identity
+{
+    using Exchangerat.Data.Models;
+
+    public interface IJwtTokenGeneratorService
+    {
+        string GenerateJwtToken(User user);
+    }
+}
