fdd102ad3cbda80d53cff770f1b0a6a19e3818e4
Yulian Ashikov <u.ashikov@abv.bg>
2020-06-21 18:15:33 +0300

    Added new component for login user form. Registered also new route for it.

---
+++ Client/src/components/identity/Login.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="col-md-6 mx-auto">
+    <h1 class="text-center">Login</h1>
+    <hr />
+    <form method="post" v-on:submit.prevent="login">
+      <div class="form-group">
+        <label for="username" class="h6">Username</label>
+        <input type="text" class="form-control" id="username" v-model="username" />
+      </div>
+
+      <div class="form-group">
+        <label for="password" class="h6">Password</label>
+        <input type="password" class="form-control" id="password" v-model="password" />
+      </div>
+
+      <input type="submit" class="btn btn-success" value="Login" />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+    data: function () {
+        return {
+            username: '',
+            password: ''
+        }
+    },
+    methods: {
+        login: function () {
+            this.$store.dispatch('login', { username: this.username, password: this.password });
+
+            this.username = '';
+            this.password = '';
+
+            this.$router.push('/');
+        }
+    }
+}
+</script>
--- Client/src/routing/router.js
+++ Client/src/routing/router.js
@@ -1,10 +1,12 @@
 import VueRouter from 'vue-router'
 
 import Register from '../components/identity/Register'
+import Login from '../components/identity/Login'
 
 const router = new VueRouter({
     routes: [
-        { path: '/users/register', name: 'register', component: Register }
+        { path: '/users/register', name: 'register', component: Register },
+        { path: '/users/login', name: 'login', component: Login }
     ],
     mode: "history"
 });
