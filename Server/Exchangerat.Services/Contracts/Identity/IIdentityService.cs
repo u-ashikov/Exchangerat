f7a1f81ac9eabd2e715b721da9ffc7cb360c894d
Yulian Ashikov <u.ashikov@abv.bg>
2020-06-21 20:50:45 +0300

    Added method for user login. Refactored the register method.

--- Server/Exchangerat.Services/Contracts/Identity/IIdentityService.cs
+++ Server/Exchangerat.Services/Contracts/Identity/IIdentityService.cs
@@ -1,11 +1,14 @@
 ﻿namespace Exchangerat.Services.Contracts.Identity
 {
+    using Exchangerat.Data.Models;
     using Exchangerat.Models.Identity;
-    using Microsoft.AspNetCore.Identity;
+    using Exchangerat.Services.Models.Common;
     using System.Threading.Tasks;
 
     public interface IIdentityService
     {
-        Task<IdentityResult> Register(RegisterUserInputModel model);
+        Task<Result<User>> Register(RegisterUserInputModel model);
+
+        Task<Result<UserOutputModel>> Login(LoginUserInputModel model);
     }
 }
--- Server/Exchangerat.Services/Implementations/Identity/IdentityService.cs
+++ Server/Exchangerat.Services/Implementations/Identity/IdentityService.cs
@@ -4,7 +4,10 @@
     using Exchangerat.Data.Models;
     using Exchangerat.Models.Identity;
     using Exchangerat.Services.Contracts.Identity;
+    using Exchangerat.Services.Models.Common;
     using Microsoft.AspNetCore.Identity;
+    using System.Collections.Generic;
+    using System.Linq;
     using System.Threading.Tasks;
 
     public class IdentityService : IIdentityService
@@ -13,16 +16,40 @@
 
         private readonly UserManager<User> userManager;
 
+        private readonly SignInManager<User> signInManager;
+
         private readonly IJwtTokenGeneratorService jwtTokenGenerator;
 
-        public IdentityService(ExchangeratDbContext dbContext, UserManager<User> userManager, IJwtTokenGeneratorService jwtTokenGenerator)
+        public IdentityService(ExchangeratDbContext dbContext, UserManager<User> userManager, SignInManager<User> signInManager, IJwtTokenGeneratorService jwtTokenGenerator)
         {
             this.dbContext = dbContext;
             this.userManager = userManager;
+            this.signInManager = signInManager;
             this.jwtTokenGenerator = jwtTokenGenerator;
         }
 
-        public async Task<IdentityResult> Register(RegisterUserInputModel model)
+        public async Task<Result<UserOutputModel>> Login(LoginUserInputModel model)
+        {
+            var existingUser = await this.userManager.FindByNameAsync(model.UserName);
+
+            if (existingUser == null)
+            {
+                return Result<UserOutputModel>.Failure(new List<string>() { "Incorrect username or password." });
+            }
+
+            var signInResult = await this.signInManager.PasswordSignInAsync(model.UserName, model.Password, isPersistent: true, lockoutOnFailure: false);
+
+            if (!signInResult.Succeeded)
+            {
+                return Result<UserOutputModel>.Failure(new List<string>() { "Incorrect username or password." });
+            }
+
+            var token = this.jwtTokenGenerator.GenerateJwtToken(existingUser);
+
+            return Result<UserOutputModel>.SuccessWith(new UserOutputModel(existingUser, token));
+        }
+
+        public async Task<Result<User>> Register(RegisterUserInputModel model)
         {
             var user = new User()
             {
@@ -35,7 +62,11 @@
 
             var identityResult = await this.userManager.CreateAsync(user, model.Password);
 
-            return identityResult;
+            var errors = identityResult.Errors.Select(e => e.Description);
+
+            return identityResult.Succeeded
+                ? Result<User>.SuccessWith(user)
+                : Result<User>.Failure(errors);
         }
     }
 }
