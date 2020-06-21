7e5e5490a7a0674fc1979fad24564c83ba4028e2
Yulian Ashikov <u.ashikov@abv.bg>
2020-06-21 18:56:25 +0300

    Added route guard before each - redirect to home if the user is authenticated and tries to reach login or register. Redirect to login if the user is not authenticated and tries to reach route different than login, register or home.

--- Client/src/routing/router.js
+++ Client/src/routing/router.js
@@ -3,6 +3,8 @@ import VueRouter from 'vue-router'
 import Register from '../components/identity/Register'
 import Login from '../components/identity/Login'
 
+import { store } from '../store/store.js'
+
 const router = new VueRouter({
     routes: [
         { path: '/users/register', name: 'register', component: Register },
@@ -10,5 +12,23 @@ const router = new VueRouter({
     ],
     mode: "history"
 });
+
+router.beforeEach(function (to, from, next) {
+    var isAuthenticated = store.getters.isAuthenticated;
+
+    if (!isAuthenticated
+        && to.path !== '/users/login'
+        && to.path !== '/users/register'
+        && to.path !== '/'
+    ) {
+        next({ name: 'login' })
+    } else {
+        if (isAuthenticated && (to.path === '/users/login' || to.path === '/users/register')) {
+            next('/');
+        } else {
+            next();
+        }
+    }
+})
 
 export { router }
