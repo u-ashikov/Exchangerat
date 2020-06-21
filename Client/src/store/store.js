fbef31efd047dcd594b57d51ee4377ceb4c2c0e0
Yulian Ashikov <u.ashikov@abv.bg>
2020-06-21 18:23:10 +0300

    Extracted the user authentication store data to external module.

--- Client/src/store/store.js
+++ Client/src/store/store.js
@@ -1,85 +1,9 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import users from '../queries/users.js'
+import userAuthModule from './modules/user-auth.js'
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
-    state: {
-        user: null,
-        token: null
-    },
-    getters: {
-        user: function (state) {
-            return state.user;
-        },
-        isAuthenticated: function (state) {
-            return state.user !== null;
-        }
-    },
-    mutations: {
-        login: function (state, userData) {
-            state.user = userData;
-            state.token = userData.token;
-        },
-        register: function (state, userData) {
-            state.user = userData.user;
-            state.token = userData.token;
-        },
-        clearUserData: function (state) {
-            state.user = null;
-            state.token = null;
-        }
-    },
-    actions: {
-        login: function (context, userData) {
-            users.login(userData)
-                .then(function (response) {
-                    if (response && response.status == 200) {
-                        console.log(response.data);
-                        context.commit('login', response.data);
-
-                        context.dispatch('setLocalStorageUserData', response.data);
-                    }
-                })
-                .catch(function (error) {
-                    console.log(error);
-                });
-        },
-        register: function (context, userData) {
-            users.register(userData)
-                .then(function (response) {
-                    if (response && response.status == 200) {
-                        console.log(response.data);
-                        context.commit('register', response.data);
-
-                        context.dispatch('setLocalStorageUserData', response.data);
-                    }
-                })
-                .catch(function (error) {
-                    // TODO: Display general error.
-                    console.log(error);
-                });
-        },
-        logout: function (context) {
-            if (context.state.user === null) {
-                return;
-            }
-
-            context.commit('clearUserData');
-
-            localStorage.clear();
-        },
-        setLocalStorageUserData: function (context, userData) {
-            // TODO: Set expiration date.
-            // var now = new Date();
-            // var expirationDate = new Date(now.getTime() + )
-
-            localStorage.setItem('userId', userData.id);
-            localStorage.setItem('token', userData.token);
-
-            // TODO: Store also the expiration.
-            // localStorage.setItem('userId', response.data.id);
-        }
-    }
+    modules: [userAuthModule]
 });
