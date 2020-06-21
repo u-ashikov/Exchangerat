011defcccb90408e345a56b4129692375f40ca78
Yulian Ashikov <u.ashikov@abv.bg>
2020-06-21 18:12:45 +0300

    Added vuex store to store user authentication data. Installed vuelidate package for validating purposes.

--- Client/src/main.js
+++ Client/src/main.js
@@ -2,14 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import { router } from './routing/router'
+import Vuelidate from 'vuelidate'
+import { store } from './store/store.js'
 
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css' 
 
 Vue.use(VueRouter);
+Vue.use(Vuelidate);
 
 new Vue({
   el: '#app',
   router,
+  store,
   render: h => h(App)
 });
