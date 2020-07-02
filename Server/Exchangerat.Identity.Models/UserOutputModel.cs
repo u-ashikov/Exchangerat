6968044a770fa10e545698a36302e1205f1e3c1c
Yulian Ashikov <u.ashikov@abv.bg>
2020-07-02 23:14:41 +0300

    Removed the Id from the UserOutputModel.

--- Server/Exchangerat.Identity.Models/UserOutputModel.cs
+++ Server/Exchangerat.Identity.Models/UserOutputModel.cs
@@ -4,13 +4,11 @@
 
     public class UserOutputModel
     {
-        public string Id { get; set; }
         public string Username { get; set; }
         public string Token { get; set; }
 
         public UserOutputModel(User user, string token)
         {
-            Id = user.Id;
             Username = user.UserName;
             Token = token;
         }
