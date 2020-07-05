d2a25163369a0f8bcabffb785ff07aa9bae1205b
u-ashikov <u.ashikov@abv.bg>
2020-07-05 17:17:00 +0300

    Admin application initial setup.

---
+++ Server/Exchangerat.Admin/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿namespace Exchangerat.Admin.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
