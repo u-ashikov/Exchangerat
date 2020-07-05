7b7e4ad437959526220bb0b9ee2ed7f5b2c1fdd1
u-ashikov <u.ashikov@abv.bg>
2020-07-05 16:00:05 +0300

    Added controller with single end point for getting all requests, registered the RequestService in the Startup.

--- Server/Exchangerat.Requests/Controllers/ExchangeratRequestsController.cs
+++ Server/Exchangerat.Requests/Controllers/ExchangeratRequestsController.cs
@@ -1,9 +1,26 @@
 ﻿namespace Exchangerat.Requests.Controllers
 {
     using Exchangerat.Controllers;
+    using Exchangerat.Requests.Services.Contracts.Requests;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Threading.Tasks;
 
     public class ExchangeratRequestsController : BaseApiController
     {
+        private readonly IRequestService requests;
 
+        public ExchangeratRequestsController(IRequestService requests)
+        {
+            this.requests = requests;
+        }
+
+        [HttpGet]
+        [Route(nameof(GetAll))]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await this.requests.GetAll();
+
+            return this.Ok(result);
+        }
     }
 }
--- Server/Exchangerat.Requests/Startup.cs
+++ Server/Exchangerat.Requests/Startup.cs
@@ -4,6 +4,8 @@ namespace Exchangerat.Requests
     using Exchangerat.Requests.Data;
     using Exchangerat.Requests.Infrastructure.Extensions;
     using Exchangerat.Requests.Services.Contracts.Identity;
+    using Exchangerat.Requests.Services.Contracts.Requests;
+    using Exchangerat.Requests.Services.Implementations.Requests;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.Extensions.Configuration;
@@ -31,6 +33,8 @@ namespace Exchangerat.Requests
                 {
                     builder.BaseAddress = new Uri("http://localhost:5001/api/Identity");
                 });
+
+            services.AddTransient<IRequestService, RequestService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
