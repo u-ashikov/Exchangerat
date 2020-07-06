e66d80a1ae1dceaf3a44551a648bbe56211ddb77
u-ashikov <u.ashikov@abv.bg>
2020-07-07 01:22:26 +0300

    Added admin common project with constants.

---
+++ Server/Exchangerat.Admin.Common/Constants/WebConstants.cs
@@ -0,0 +1,22 @@
+﻿namespace Exchangerat.Admin.Common.Constants
+{
+    public static class WebConstants
+    {
+        public const string AuthCookieKey = "ExchangeRatAuthCookie";
+
+        public static class Controllers
+        {
+            public const string Home = "Home";
+        }
+
+        public static class Actions
+        {
+            public const string Index = "Index";
+        }
+
+        public static class Messages
+        {
+            public const string GeneralError = "Sorry, something went wrong.";
+        }
+    }
+}
