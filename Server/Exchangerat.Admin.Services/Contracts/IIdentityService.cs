6b2c2f25372143f36b41e76d3269d1c912b63550
u-ashikov <u.ashikov@abv.bg>
2020-07-06 23:21:40 +0300

    Added admin services project with identity service for making requests to the identity micro service.

---
+++ Server/Exchangerat.Admin.Services/Contracts/IIdentityService.cs
@@ -0,0 +1,12 @@
+﻿namespace Exchangerat.Admin.Services.Contracts
+{
+    using Exchangerat.Admin.Models.Models.Identity;
+    using Refit;
+    using System.Threading.Tasks;
+
+    public interface IIdentityService
+    {
+        [Post("/Login")]
+        Task<UserOutputModel> Login([Body]LoginFormModel model);
+    }
+}
