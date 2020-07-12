9a572893f7be6c67eb0b1447ea5ab223ef6fbfe1
Yulian Ashikov <u.ashikov@abv.bg>
2020-07-12 22:45:24 +0300

    Added front-end pagination to the user's funds listing.

--- Client/src/components/funds/MyFunds.vue
+++ Client/src/components/funds/MyFunds.vue
@@ -1,26 +1,32 @@
 <template>
     <div>
-      <div v-show="funds && funds.length > 0">
-        <h1 class="display-4 text-center mt-3 mb-5">My Funds</h1>
-        <table class="container table table-striped">
-            <thead>
-                <th>Card Identity Number</th>
-                <th>Receiver Account</th>
-                <th>Amount</th>
-                <th>Issued At</th>
-            </thead>
-            <tbody>
-                <tr v-for="fund in funds" v-bind:key="fund.issuedAt">
-                    <td>{{ fund.cardIdentityNumber}}</td>
-                    <td>
-                        <router-link tag="a" :to="{ name: 'accountTransactions', params: { accountId: fund.accountId }}">{{ fund.accountIdentityNumber }}</router-link>
-                    </td>
-                    <td>&dollar; {{ fund.amount | money }}</td>
-                    <td>{{ fund.issuedAt | transactionDate }}</td>
-                </tr>
-            </tbody>
-        </table>
-      </div>
+      <pagination v-bind:list-data="funds">
+          <template #heading>
+              <h1 class="display-4 text-center mt-3 mb-5">My Funds</h1>
+          </template>
+          <template #data="{ paginatedData }">
+              <div v-show="paginatedData && paginatedData.length > 0">
+                <table class="container table table-striped">
+                    <thead>
+                        <th>Card Identity Number</th>
+                        <th>Receiver Account</th>
+                        <th>Amount</th>
+                        <th>Issued At</th>
+                    </thead>
+                    <tbody>
+                        <tr v-for="fund in funds" v-bind:key="fund.issuedAt">
+                            <td>{{ fund.cardIdentityNumber}}</td>
+                            <td>
+                                <router-link tag="a" :to="{ name: 'accountTransactions', params: { accountId: fund.accountId }}">{{ fund.accountIdentityNumber }}</router-link>
+                            </td>
+                            <td>&dollar; {{ fund.amount | money }}</td>
+                            <td>{{ fund.issuedAt | transactionDate }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+              </div>
+          </template>
+      </pagination>
       <div v-show="!funds || funds.length === 0 && errors.length === 0" class="container w-50 alert alert-success mx-auto my-5" role="alert" v-cloak>
         <h4 class="alert-heading">Ooooops!</h4>
         <p>Sorry, you don't have any funds yet. If you want you can add some by using the link bellow.</p>
@@ -34,17 +40,19 @@
 </template>
 
 <script>
-import ValidationError from '../../components/shared/ValidationError';
+import ValidationError from '../../components/shared/ValidationError'
+import Pagination from '../../components/shared/Pagination'
 
-import fundService from "../../services/fundService";
-import errorHandler from "../../helpers/error-handler";
+import fundService from "../../services/fundService"
+import errorHandler from "../../helpers/error-handler"
 
-import moment from 'moment';
-import numeral from 'numeral';
+import moment from 'moment'
+import numeral from 'numeral'
 
 export default {
   components: {
-      validationError: ValidationError
+      validationError: ValidationError,
+      pagination: Pagination
   },
   data: function() {
     return {
@@ -71,7 +79,6 @@ export default {
       .getMy()
       .then(function(response) {
         if (response && response.data) {
-            console.log(response.data);
             self.funds = response.data;
         }
       })
