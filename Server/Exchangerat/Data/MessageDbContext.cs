e40b39080ef57bf80fd56f68ea8731a932fcac75
Yulian Ashikov <u.ashikov@abv.bg>
2020-07-14 23:28:39 +0300

    Added Message db model, db context and configuration for storing messages sent between micro services and their status.

---
+++ Server/Exchangerat/Data/MessageDbContext.cs
@@ -0,0 +1,26 @@
+﻿namespace Exchangerat.Data
+{
+    using Configurations;
+    using Microsoft.EntityFrameworkCore;
+    using Models;
+    using System.Reflection;
+
+    public abstract class MessageDbContext : DbContext
+    {
+        protected MessageDbContext(DbContextOptions options)
+            :base(options) { }
+
+
+        public DbSet<Message> Messages { get; set; }
+
+        protected abstract Assembly ConfigurationsAssembly { get; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new MessageConfiguration());
+            modelBuilder.ApplyConfigurationsFromAssembly(this.ConfigurationsAssembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Server/Exchangerat/Data/Models/Message.cs
@@ -0,0 +1,40 @@
+﻿namespace Exchangerat.Data.Models
+{
+    using Newtonsoft.Json;
+    using System;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Message
+    {
+        private string serializedData;
+
+        public Message(object data)
+        {
+            this.Data = data;
+        }
+
+        private Message() { }
+
+        public int Id { get; private set; }
+
+        public Type Type { get; private set; }
+
+        public bool Published { get; private set; }
+
+        public void MarkAsPublished() => this.Published = true;
+
+        [NotMapped]
+        public object Data
+        { // Make sure you ignore null values!
+            get => JsonConvert.DeserializeObject(this.serializedData, this.Type, 
+                new JsonSerializerSettings() { NullValueHandling = NullValueHandling.Ignore });
+            set
+            {
+                this.Type = value.GetType();
+
+                this.serializedData = JsonConvert.SerializeObject(value);
+            }
+        }
+
+    }
+}
