78f6c40e19e1caafd5d7ffd10aca11631d8abbea
Yulian Ashikov <u.ashikov@abv.bg>
2020-07-14 23:30:57 +0300

    Added new project in the admin with AdminDbContext with table for sent messages. Added migration.

---
+++ Server/Exchangerat.Admin.Common/Enums/Status.cs
@@ -0,0 +1,9 @@
+﻿namespace Exchangerat.Admin.Common.Enums
+{
+    public enum Status
+    {
+        Pending = 0,
+        Approved = 1,
+        Cancelled = 2
+    }
+}
---
+++ Server/Exchangerat.Admin.Data/AdminDbContext.cs
@@ -0,0 +1,14 @@
+﻿namespace Exchangerat.Admin.Data
+{
+    using Exchangerat.Data;
+    using Microsoft.EntityFrameworkCore;
+    using System.Reflection;
+
+    public class AdminDbContext : MessageDbContext
+    {
+        public AdminDbContext(DbContextOptions<AdminDbContext> options) 
+            : base(options) { }
+
+        protected override Assembly ConfigurationsAssembly => Assembly.GetExecutingAssembly();
+    }
+}
