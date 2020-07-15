eaf672b3ba8c01ef48cde96916ec324dee825489
Yulian Ashikov <u.ashikov@abv.bg>
2020-07-15 19:16:49 +0300

    Created a background worker to process pending messages.

---
+++ Server/Exchangerat/Messages/MessagesHostedService.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Exchangerat.Messages
+{
+    using Data.Models;
+    using Hangfire;
+    using MassTransit;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Hosting;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class MessagesHostedService : IHostedService
+    {
+        private readonly IServiceScopeFactory scopeFactory;
+
+        private readonly IRecurringJobManager recurringJob;
+
+        private readonly IBus publisher;
+
+        public MessagesHostedService(IRecurringJobManager recurringJob, IBus publisher, IServiceScopeFactory scopeFactory)
+        {
+            this.recurringJob = recurringJob;
+            this.publisher = publisher;
+            this.scopeFactory = scopeFactory;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            this.recurringJob
+                .AddOrUpdate(nameof(MessagesHostedService),
+                    () => this.ProcessPendingMessages(),
+                    "*/5 * * * * *");
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+
+        public void ProcessPendingMessages()
+        {
+            using var scope = this.scopeFactory.CreateScope();
+
+            var dbContext = scope.ServiceProvider.GetRequiredService<DbContext>();
+
+            var messages = dbContext
+                .Set<Message>()
+                .Where(m => !m.Published)
+                .OrderBy(m => m.Id)
+                .ToList();
+
+            foreach (var message in messages)
+            {
+                this.publisher.Publish(message.Data, message.Type);
+
+                message.MarkAsPublished();
+
+                dbContext.SaveChanges();
+            }
+        }
+    }
+}
